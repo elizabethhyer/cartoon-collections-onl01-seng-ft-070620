54517a666e36f7c2e264714c0319f30764ea5f4a
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 12:35:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,4 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(items)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  items.find do |item|
-    when items != item 
-      puts nil
-  end
-  [cheese].include?(find_the_cheese) 
-end
+

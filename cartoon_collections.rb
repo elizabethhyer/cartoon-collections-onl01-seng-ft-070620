17935839e80320912aa5e172d85df3b5a103e9e5
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 12:25:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarf_names)
+ dwarf_names.each_with_index do |name, num|
+   puts "#{num+1} #{name}"
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |calls|
+    "#{calls.capitalize}!"
+  end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |calls|
+    calls.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  items.find do |item|
+    when items != item 
+      puts nil
+  end
+  cheese_types.include?("item") 
 end
